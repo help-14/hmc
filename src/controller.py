3763bdc12d80dccd59c267e82480082dad16ccee
Help-14 <help-14@hotmail.com>
2019-07-05 14:57:48 +0700

    socket io python

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,29 @@
 from __future__ import print_function
 from inputs import get_gamepad
+import socketio
+import inputs
+
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    print("connection established")
+
+
+@sio.event
+def my_message(data):
+    print("message received with ", data)
+    sio.emit("my response", {"response": "my response"})
+
+
+@sio.event
+def disconnect():
+    print("disconnected from server")
+
+
+sio.connect("http://localhost:3000")
+sio.wait()
 
 
 def main():
@@ -8,6 +32,8 @@ def main():
         events = get_gamepad()
         for event in events:
             print(event.ev_type, event.code, event.state)
+            if event.ev_type == "Absolute" and event.state == 0:
+                sio.emit("gamepad", event.code)
 
 
 if __name__ == "__main__":
